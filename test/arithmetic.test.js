0581cf4ffe205e6da95eae65ef47e6c9e09ca744
Kothagundu Udaya Praneeth Gupta <163750158+praneeth00007@users.noreply.github.com>
2025-12-15 09:44:49 +0000

    Tests: add coverage for power operation

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -94,6 +94,40 @@ describe('Arithmetic', function () {
     });
 
 // TODO: Challenge #1
+    describe('Power', function () {
+        it('raises a positive integer to a positive integer exponent', function (done) {
+            request.get('/arithmetic?operation=power&operand1=2&operand2=3')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 8 });
+                    done();
+                });
+        });
+        it('returns 1 when exponent is zero', function (done) {
+            request.get('/arithmetic?operation=power&operand1=42&operand2=0')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 1 });
+                    done();
+                });
+        });
+        it('handles negative exponents', function (done) {
+            request.get('/arithmetic?operation=power&operand1=2&operand2=-3')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 0.125 });
+                    done();
+                });
+        });
+        it('handles negative bases', function (done) {
+            request.get('/arithmetic?operation=power&operand1=-2&operand2=3')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: -8 });
+                    done();
+                });
+        });
+    });
  
 
     describe('Multiplication', function () {
